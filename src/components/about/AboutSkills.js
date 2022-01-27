0164633fccf8424e9a38157bdd6cc69a1bb83d79
Nunsinee Viken <71260513+nunsinee@users.noreply.github.com>
2022-01-27 12:26:55 +0100

    update Img

--- src/components/about/AboutSkills.js
+++ src/components/about/AboutSkills.js
@@ -1,12 +1,12 @@
 import React from "react"
-import AboutmeImg from "../../assets/nunsinee-viken-about.jpg"
+import Aboutme from "../../assets/nunsinee-viken-about.jpg"
 
 function AboutSkills() {
 	return (
 		<div className="col-lg-6 col-sm-12">
 			<div className="box-about">
 				<img
-					src={AboutmeImg}
+					src={Aboutme}
 					alt="nunsinee viken photo"
 					className="img-fluid img-thumbnail"
 				/>
